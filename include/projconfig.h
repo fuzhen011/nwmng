111e0aa4ddd4c79ba01b06ef6a4d8dcb9b0b0400
ZhenFu <zhfu@silabs.com>
2020-01-09 09:36:45 +0800

    add projconfig.h.in, try to see if the projconfig.h will be overwritten or just append

--- include/projconfig.h
+++ include/projconfig.h
@@ -14,12 +14,9 @@ extern "C"
 #ifndef SINGLE_PROC
 #define SINGLE_PROC
 #endif
+
 #define FILE_PATH_MAX 108
-
 #define MAXSLEEP 128
-#define GC_EXP_DEV_NUM  128
-
-#define TMPLATE_FILE_PATH PROJ_DIR "tools/mesh_config/templates.json"
 
 #if __APPLE__ == 1
 #define PROJ_DIR  "/Users/zhfu/work/projs/nwmng/"
@@ -34,18 +31,13 @@ extern "C"
 #define NWNODES_FILE_PATH PROJ_DIR "tools/mesh_config/test1/nwk.json"
 #endif
 
+#define TMPLATE_FILE_PATH PROJ_DIR "tools/mesh_config/templates.json"
 #define CLI_LOG_FILE_PATH PROJ_DIR "logs/cli.log"
 
 #ifndef CLIENT_ENCRYPTED_PATH
 #define CLIENT_ENCRYPTED_PATH                       "/tmp/client_encrypted"
 #endif
 #define ENC_SOCK_FILE_PATH                          "/tmp/enc_sock"
-
-/*
- * Socket file path for communication between cli-mng and cfg processes.
- */
-#define CC_SOCK_SERV_PATH "/tmp/ccsock_serv"
-#define CC_SOCK_CLNT_PATH "/tmp/ccsock_clnt"
 
 /*
  * NOTE: Make sure this value is NOT greater than the Max Prov Sessions
@@ -71,7 +63,8 @@ extern "C"
 #define CONFIG_NO_RSP_TIMEOUT 3
 
 /*
- * Retry times
+ * Retry times - each config client commands may fail with reasons, retry is
+ * implemented, this definitions decide how many times to retry before failure
  */
 #define GET_DCD_RETRY_TIMES 5
 #define ADD_APP_KEY_RETRY_TIMES 5
